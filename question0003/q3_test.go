4721059804ed43c28d85ebd70c46f4c802be460a
remilelei <remilelei@tencent.com>
2020-10-15 16:51:28 +0800

    [AC] longest-substring-without-repeating-characters 3

---
+++ question0003/q3_test.go
@@ -0,0 +1,66 @@
+package question0003
+
+import "testing"
+
+func TestQuestion3_1(t *testing.T) {
+	data := "bbbbb"
+	res := lengthOfLongestSubstring(data)
+	answer := 1
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_2(t *testing.T) {
+	data := "abcabcbb"
+	res := lengthOfLongestSubstring(data)
+	answer := 3
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_3(t *testing.T) {
+	data := "pwwkew"
+	res := lengthOfLongestSubstring(data)
+	answer := 3
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_4(t *testing.T) {
+	data := "aab"
+	res := lengthOfLongestSubstring(data)
+	answer := 2
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_5(t *testing.T) {
+	data := "dvdf"
+	res := lengthOfLongestSubstring(data)
+	answer := 3
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_6(t *testing.T) {
+	data := "dvdfabcdad"
+	res := lengthOfLongestSubstring(data)
+	answer := 6
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
+
+func TestQuestion3_7(t *testing.T) {
+	data := "abba"
+	res := lengthOfLongestSubstring(data)
+	answer := 2
+	if res != answer {
+		t.Fatalf("[%v] answer=%v but res=%v", data, answer, res)
+	}
+}
